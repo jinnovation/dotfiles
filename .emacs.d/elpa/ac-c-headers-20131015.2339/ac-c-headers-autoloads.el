2471449b8c4c0c2f71fa8c7846dddb173ecd87ab
Jonathan Jin <jjin082693@gmail.com>
2014-02-26 20:59:53 -0600

    new pkgs

---
+++ .emacs.d/elpa/ac-c-headers-20131015.2339/ac-c-headers-autoloads.el
@@ -0,0 +1,18 @@
+;;; ac-c-headers-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("ac-c-headers-pkg.el" "ac-c-headers.el")
+;;;;;;  (21261 1551 42430 736000))
+
+;;;***
+
+(provide 'ac-c-headers-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ac-c-headers-autoloads.el ends here
--- .emacs.d/elpa/solarized-theme-20140217.32/solarized-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "solarized-theme" "20140217.32" "The Solarized color theme, ported to Emacs." 'nil)
