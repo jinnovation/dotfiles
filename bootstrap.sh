e6362e949ef19a4548a4db10334baf9835d523e3
Jonathan Jin <jjin082693@gmail.com>
2014-11-19 10:51:53 -0600

    add to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,15 @@
 HOMEDIR=/home/jjin/
 DOTDIR=$HOMEDIR/dotfiles
 
-ln -s $DOTDIR/vimrc $HOMEDIR/.vimrc
-ln -s $DOTDIR/gvimrc $HOMEDIR/.gvimrc
+VIMRC=vimrc
+GVIMRC=gvimrc
+
+ln -s $DOTDIR/$VIMRC $HOMEDIR/.vimrc
+ln -s $DOTDIR/$GVIMRC $HOMEDIR/.gvimrc
 
 mkdir $HOMEDIR/.vim
 ln -s $DOTDIR/vim/fn.vim $HOMEDIR/.vim/fn.vim
 ln -s $DOTDIR/vim/plug.vim $HOMEDIR/.vim/plug.vim
+
+ln -s $DOTDIR/.Xresources $HOMEDIR/.Xresources
+ln -s $DOTDIR/tmux.conf $HOMEDIR/tmux.conf
