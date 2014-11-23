0ec91b7bfeb9213ef62fb3af58bc5c5a5e2e841e
Jonathan Jin <jjin082693@gmail.com>
2014-11-22 22:09:40 -0600

    cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,6 @@
 HOMEDIR=/home/jjin/
 DOTDIR=$HOMEDIR/dotfiles
 
-VIMRC=vimrc
-GVIMRC=gvimrc
-
 if [ -e $HOMEDIR/.vim ]
 then
   echo "Moving old .vim/ to .vim.old/"
@@ -12,25 +9,31 @@ fi
 mkdir $HOMEDIR/.vim
 
 link_pairs=(
-        $DOTDIR/vimrc .vimrc
-        $DOTDIR/gvimrc .gvimrc
-        $DOTDIR/vim/fn.vim .vim/fn.vim
-        $DOTDIR/vim/plug.vim .vim/plug.vim
-        $DOTDIR/.Xresources .Xresources
-        $DOTDIR/tmux.conf tmux.conf
-        $DOTDIR/mutt/muttrc .muttrc
-        $DOTDIR/mutt/signature .signature
-        $DOTDIR/bashrc .bashrc
-        $DOTDIR/gitconfig .gitconfig
-        $DOTDIR/gitignore_global .gitignore_global
+        $DOTDIR/vimrc              .vimrc
+        $DOTDIR/gvimrc             .gvimrc
+        $DOTDIR/vim/fn.vim         .vim/fn.vim
+        $DOTDIR/vim/plug.vim       .vim/plug.vim
+        $DOTDIR/.Xresources        .Xresources
+        $DOTDIR/tmux.conf          tmux.conf
+        $DOTDIR/mutt/muttrc        .muttrc
+        $DOTDIR/mutt/signature     .signature
+        $DOTDIR/bashrc             .bashrc
+        $DOTDIR/gitconfig          .gitconfig
+        $DOTDIR/gitignore_global   .gitignore_global
 )
 
-for (( i = 0 ; i < ${#link_pairs[@]} ; i+=2)) do
+old_dotfiles=()
+
+for (( i=0 ; i < ${#link_pairs[@]} ; i+=2 )) do
   if [ -e $HOMEDIR/${link_pairs[$i+1]} ]
   then
     echo "Moving old ${link_pairs[$i+1]} to ${link_pairs[$i+1]}.old"
     mv ${link_pairs[$i+1]} ${link_pairs[$i+1]}.old
+
+    old_dotfiles+=(${link_pairs[$i+1]}.old)
   fi
 
   ln -s ${link_pairs[$i]} $HOMEDIR/${link_pairs[$i+1]}
 done
+
+# TODO: optionally delete *.old files 
