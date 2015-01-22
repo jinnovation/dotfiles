48e1342179ca80cb95ff13a9ccd84a64d6b686ac
Jonathan Jin <jjin082693@gmail.com>
2015-01-22 16:07:31 -0600

    use native /home/jjin var

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-HOMEDIR=/home/jjin/
-DOTDIR=$HOMEDIR/dotfiles
+DOTDIR=$HOME/dotfiles
 
-if [ -e $HOMEDIR/.vim ]
+if [ -e $HOME/.vim ]
 then
   echo "Moving old .vim/ to .vim.old/"
-  mv $HOMEDIR/.vim/ $HOMEDIR/.vim.old
+  mv $HOME/.vim/ $HOME/.vim.old
 fi
-mkdir $HOMEDIR/.vim
-mkdir $HOMEDIR/.vim/autoload
+mkdir $HOME/.vim
+mkdir $HOME/.vim/autoload
 
 # TODO: if vim-plug is empty, exit with note to init submodule
 #       execute PlugInstall on vim
@@ -35,7 +34,7 @@ link_pairs=(
 old_dotfiles=()
 
 for (( i=0 ; i < ${#link_pairs[@]} ; i+=2 )) do
-  if [ -e $HOMEDIR/${link_pairs[$i+1]} ]
+  if [ -e $HOME/${link_pairs[$i+1]} ]
   then
     echo "Moving old ${link_pairs[$i+1]} to ${link_pairs[$i+1]}.old"
     mv ${link_pairs[$i+1]} ${link_pairs[$i+1]}.old
@@ -43,7 +42,7 @@ for (( i=0 ; i < ${#link_pairs[@]} ; i+=2 )) do
     old_dotfiles+=(${link_pairs[$i+1]}.old)
   fi
 
-  ln -s ${link_pairs[$i]} $HOMEDIR/${link_pairs[$i+1]}
+  ln -s ${link_pairs[$i]} $HOME/${link_pairs[$i+1]}
 done
 
 # TODO: optionally delete *.old files
